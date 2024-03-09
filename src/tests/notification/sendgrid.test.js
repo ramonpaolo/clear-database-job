867e8c710e51fe22875bdeb67b42f5845a794ffc
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2024-03-09 19:06:05 -0300

    tests: change strict to equal

--- src/tests/notification/sendgrid.test.js
+++ src/tests/notification/sendgrid.test.js
@@ -2,7 +2,7 @@ import { describe, it } from "node:test";
 import fs from 'node:fs/promises'
 import { resolve } from "node:path";
 import { cwd } from "node:process";
-import { doesNotThrow, ok, strict } from "node:assert";
+import { doesNotThrow, equal, ok } from "node:assert";
 
 import sendgrid from '@sendgrid/mail';
 
@@ -18,7 +18,7 @@ describe(providerTitle, () => {
     it(`the total of template inside ${provider} need be 1`, async () => {
       const files = await fs.readdir(pathTemplateDir)
 
-      strict(files.length, 1)
+      equal(files.length, 1)
     })
 
     const templates = ['success.html'];
@@ -37,8 +37,8 @@ describe(providerTitle, () => {
         const expectedFile = (await fs.readFile(resolve(pathTemplateDir, template))).toString()
         const receivedFile = selectTemplate(template.split('.')[0])
 
-        strict(receivedFile.length, expectedFile.length)
-        strict(receivedFile, expectedFile)
+        equal(receivedFile.length, expectedFile.length)
+        equal(receivedFile, expectedFile)
       })
     )
   })
@@ -58,7 +58,7 @@ describe(providerTitle, () => {
 
       await main('success', info);
 
-      strict(tracker.mock.callCount(), 1)
+      equal(tracker.mock.callCount(), 1)
       ok(tracker.mock.calls[0].arguments[0].html.includes(`<strong>${info.deleted_documents}</strong>`))
       ok(tracker.mock.calls[0].arguments[0].html.includes(`<strong>${(info.end_time - info.start_time).toFixed(2)}</strong>`))
       ok(tracker.mock.calls[0].arguments[0].html.includes(`<code>${JSON.stringify(info.query, {}, 2)}</code>`))
